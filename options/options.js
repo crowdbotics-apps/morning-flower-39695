d6d7d40cc6546ea1958de884e51262b10404dee9
Crowdbotics <team@crowdbotics.com>
2023-04-04 01:41:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "morning-flower-39695",
+  url: "https://morning-flower-39695.botics.co",
+  api: "https://morning-flower-39695.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
